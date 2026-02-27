c02983fee6e58379198db5e0081f55f9d4d172dd
Trevor Durán <trevor.m.duran@gmail.com>
2026-02-27 15:01:10 -0600

    feat: set helper methods as private

--- lib/bob.rb
+++ lib/bob.rb
@@ -1,39 +1,43 @@
 class Bob
-  def self.hey(remark)
-    remark = remark.strip # strip whitespace
+  class << self
+    def hey(remark)
+      remark = remark.strip # strip whitespace
 
-    if remark[-1] == '?' || is_elevated_question(remark)
-      if is_yelling(remark)
-        return "Calm down, I know what I'm doing!"
+      if remark[-1] == '?' || is_elevated_question(remark)
+        if is_yelling(remark)
+          return "Calm down, I know what I'm doing!"
+        end
+        return 'Sure.'
       end
-      return 'Sure.'
+
+      if is_yelling(remark)
+        return 'Whoa, chill out!'
+      end
+
+      if remark.empty?
+        return 'Fine. Be that way!'
+      end
+
+      'Whatever.'
     end
 
-    if is_yelling(remark)
-      return 'Whoa, chill out!'
+    private
+
+    def is_yelling(str)
+      return str.upcase == str && str.match(/[A-Z]/)
     end
 
-    if remark.empty?
-      return 'Fine. Be that way!'
+    def get_num_ending_exclamations(str)
+      match = str.match(/(!+)$/)
+      if match
+        return match[1].length
+      end
+      return 0
     end
 
-    'Whatever.'
-  end
-
-  def self.is_yelling(str)
-    return str.upcase == str && str.match(/[A-Z]/)
-  end
-
-  def self.get_num_ending_exclamations(str)
-    match = str.match(/(!+)$/)
-    if match
-      return match[1].length
+    def is_elevated_question(str)
+      num_ending_exclamations = get_num_ending_exclamations(str)
+      return num_ending_exclamations > 0 && str[-(num_ending_exclamations + 1)] == '?'
     end
-    return 0
-  end
-
-  def self.is_elevated_question(str)
-    num_ending_exclamations = get_num_ending_exclamations(str)
-    return num_ending_exclamations > 0 && str[-(num_ending_exclamations + 1)] == '?'
   end
 end
